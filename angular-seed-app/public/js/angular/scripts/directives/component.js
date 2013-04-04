30bd12694ae49881b0c32089fcf81592aa4f79bd
Bhaskara Kempaiah <bhaskara@ubermedia.com>
2013-04-04 14:26:57 +0530

    minor text related changes

--- angular-seed-app/public/js/angular/scripts/directives/component.js
+++ angular-seed-app/public/js/angular/scripts/directives/component.js
@@ -16,18 +16,6 @@ componentModule.directive('createUser', function(user) {
         elm.find('#ui_createuser').modal();
       }
 
-      //invokes the service layer to make the AJAX call
-      scope.addDonor2 = function(){
-        //send the new user object to service layer
-        user.create(scope.new_user, function(data){
-          //on response, append the newly created user to list
-          scope.donorUsers.push(data);
-
-          //since we used a popup to create user form, close it
-          scope.closeForm();
-        });
-      }
-
       //used for handling close & anything else
       scope.closeForm = function(){
         elm.find('#ui_createuser').modal('hide');
